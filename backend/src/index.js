9c6246be1eb4144184f00400a175c70644da32ef
gabrielcancio <gabriel.cancio02@gmail.com>
2020-05-15 23:51:55 -0300

    Starting routes system

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,4 +3,12 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.get('/users', (request, response) => {
+    return response.json({
+        message: "Hello World"
+    })
+})
+
 app.listen(3333);
+
+// Video (21:32)
