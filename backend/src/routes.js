8f8b1a33a66803f40e7b1a5f3108fc3089fd8226
gabrielcancio <gabriel.cancio02@gmail.com>
2020-05-16 12:23:47 -0300

    Configuring the mongoDB acess on the application

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,29 @@
 const express = require('express');
+const axios = require('axios');
 
 const routes = express.Router();
+const Dev = require('./models/Dev');
+
+routes.post('/devs', async (request, response) => {
+    const { github_username, techs  } = (request.body);
+
+    const githubApiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+    const { name = login, avatar_url, bio } = githubApiResponse.data;
+
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray
+    })
+
+    console.log(name, avatar_url, bio);
+
+    return response.json(dev);
+})
 
 module.exports = routes;
